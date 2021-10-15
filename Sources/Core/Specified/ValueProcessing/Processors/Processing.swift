2015bd316bf48ab5b5718aaa6919a276b52bca7f
Asio Otus <asio.otus.verus@icloud.com>
2021-10-15 16:33:20 +0300

    Join Modifier and Processing

--- Sources/Core/Specified/ValueProcessing/Processors/Processing.swift
+++ Sources/Core/Specified/ValueProcessing/Processors/Processing.swift
@@ -1,57 +1,24 @@
-extension Processing {
-	public enum ActionResult {
-		case success(Value)
-		case failure
-		
-		public init (_ bool: Bool, _ value: Value) {
-			self = bool ? .success(value) : .failure
-		}
-	}
-}
-
-public extension ProcessingResult {
-	init (_ processingResult: Processing<Value, Failure>.ActionResult, _ value: Value, _ failure: Failure, _ label: String? = nil) {
-		switch processingResult {
-		case let .success(value):
-			self = .single(.init(.success(value), Processing<Value, Failure>.name, label))
-			
-		case .failure:
-			self = .single(.init(.failure(failure), Processing<Value, Failure>.name, label))
-		}
-	}
-}
-
 public struct Processing <Value, Failure>: ProcessorProtocol {
 	public static var name: String { "processing" }
 	
 	public let label: String?
 	
-	public let processing: (Value) -> ActionResult
-	public let failure: Failure
+	public let processing: (Value) -> Value
 	
-	public init (label: String? = nil, failure: Failure, processing: @escaping (Value) -> ActionResult) {
+	public init (label: String? = nil, processing: @escaping (Value) -> Value) {
 		self.label = label
-		self.failure = failure
 		self.processing = processing
 	}
 	
-	public init (label: String? = nil, failure: Failure, processing: @escaping (Value) -> Value) {
-		self.init(label: label, failure: failure, processing: { .success(processing($0)) })
-	}
-	
 	public func process (_ value: Value) -> ProcessingResult<Value, Failure> {
-		let actionResult = processing(value)
-		return .init(actionResult, value, failure, label)
+		let processedValue = processing(value)
+		return .single(.init(.success(processedValue), Self.name, label))
 	}
 }
 
 public extension AnyProcessor {
-	static func process (label: String? = nil, failure: Failure, processing: @escaping (Value) -> Processing<Value, Failure>.ActionResult) -> Self {
-		Processing(label: label, failure: failure, processing: processing).eraseToAnyProcessor()
-	}
-	
-	static func process (label: String? = nil, failure: Failure, processing: @escaping (Value) -> Value) -> Self {
-		Processing(label: label, failure: failure, processing: processing).eraseToAnyProcessor()
+	static func process (label: String? = nil, processing: @escaping (Value) -> Value) -> Self {
+		Processing(label: label, processing: processing).eraseToAnyProcessor()
 	}
 	
 	static func process (_ processing: Processing<Value, Failure>) -> Self {
