8bc31f47af943ab3a5d664021f435df8da61beff
Asio Otus <asio.otus.verus@icloud.com>
2021-10-21 17:11:09 +0300

    Upgrade Echo class

--- Sources/Core/Common/Classes/Echo.swift
+++ Sources/Core/Common/Classes/Echo.swift
@@ -1,10 +1,24 @@
 import Foundation
 
 extension Echo {
-	public struct Model {
-		public let info: [String: String]?
+	public struct Model: Codable {
+		public static let dateFormatter: DateFormatter = {
+			let dateFormatter = DateFormatter()
+			dateFormatter.dateFormat = "yyyy.MM.dd HH:mm:ss.SSSS"
+			return dateFormatter
+		}()
+		
 		public let date: String
+		public let timestamp: Double
+		public let info: [String: String]?
 		public let processId: String
+		
+		public init (date: Date, info: [String: String]?, processId: String) {
+			self.date = Self.dateFormatter.string(from: date)
+			self.timestamp = date.timeIntervalSince1970
+			self.info = info
+			self.processId = processId
+		}
 	}
 	
 	struct Counter {
@@ -20,16 +34,14 @@ extension Echo {
 public class Echo {
 	public static let standard = Echo()
 	
-	static let dateFormatter: DateFormatter = {
-		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "HH:mm:ss dd.MM.yyyy"
-		return dateFormatter
-	}()
-	
 	var counter = Counter()
 }
 
 public extension Echo {
+	func ping () -> [String: Model] {
+		["ping": model()]
+	}
+	
 	func echo (_ phrase: String? = nil) -> [String: Model] {
 		if let phrase = phrase {
 			return ["echo": model(["phrase": phrase])]
@@ -38,31 +50,21 @@ public extension Echo {
 		}
 	}
 	
-	func test () -> [String: Model] {
-		["test": model()]
-	}
-	
 	func pathTest (_ path: String) -> [String: Model] {
 		["test": model(["path": path])]
 	}
 	
 	func count () -> [String: Model] {
-		["count": model(["count": counter.next()])]
+		["count": model(["number": counter.next()])]
 	}
 }
 
 private extension Echo {
-	var date: String {
-		let currentDate = Date()
-		let formattedDate = Self.dateFormatter.string(from: currentDate)
-		return formattedDate
-	}
-	
-	var processId: String {
-		String(ProcessInfo.processInfo.processIdentifier)
-	}
-	
 	func model (_ info: [String: String]? = nil) -> Model {
-		.init(info: info, date: date, processId: processId)
+		.init(
+			date: Date(),
+			info: info,
+			processId: String(ProcessInfo.processInfo.processIdentifier)
+		)
 	}
 }
