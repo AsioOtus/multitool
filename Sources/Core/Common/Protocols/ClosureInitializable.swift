39c5216dd0ed1fa1914284d7291ec43c4c442d66
Asio Otus <asio.otus.verus@icloud.com>
2022-02-05 20:35:25 +0200

    Update customisable protocol

--- Sources/Core/Common/Protocols/ClosureInitializable.swift
+++ Sources/Core/Common/Protocols/ClosureInitializable.swift
@@ -6,3 +6,10 @@ public extension ClosureInitializable where Self: EmptyInitializable {
 		try block(&self)
 	}
 }
+
+public extension ClosureInitializable where Self: AnyObject, Self: EmptyInitializable {
+	init (_ block: (Self) throws -> Void) rethrows {
+		self.init()
+		try block(self)
+	}
+}
--- Sources/Core/Common/Protocols/Customizable.swift
+++ Sources/Core/Common/Protocols/Customizable.swift
@@ -1,43 +1,71 @@
 public protocol Customizable { }
 
 public extension Customizable {
-	func use (in block: (Self) throws -> Void) rethrows {
+	func `do` (_ block: (Self) throws -> Void) rethrows {
 		try block(self)
 	}
 	
-	func useCopy (in block: (inout Self) throws -> Void) rethrows {
+	func use (_ block: (inout Self) throws -> Void) rethrows {
 		var selfCopy = self
 		try block(&selfCopy)
 	}
-	
-	mutating func useSelf (in block: (inout Self) throws -> Void) rethrows {
-		try block(&self)
-	}
-}
 
-public extension Customizable {
-	func set (in block: (inout Self) throws -> Void) rethrows -> Self {
+	func set (_ block: (inout Self) throws -> Void) rethrows -> Self {
 		var selfCopy = self
 		try block(&selfCopy)
 		return selfCopy
 	}
 	
-	mutating func setSelf (in block: (inout Self) throws -> Void) rethrows -> Self {
+	mutating func mutate (_ block: (inout Self) throws -> Void) rethrows {
 		try block(&self)
-		return self
+	}
+	
+	func `let` <T> (_ block: (Self) throws -> T) rethrows -> T {
+		try block(self)
 	}
 }
 
 public extension Customizable where Self: AnyObject {
-	func set (in block: (Self) throws -> Void) rethrows -> Self {
+	func set (_ block: (Self) throws -> Void) rethrows -> Self {
 		try block(self)
 		return self
 	}
 }
 
-public extension Customizable {
-	func transform <T> (in block: (Self) throws -> T) rethrows -> T {
-		let result = try block(self)
-		return result
-	}
-}
+
+
+
+
+extension Array:      Customizable { }
+extension Dictionary: Customizable { }
+extension Set:        Customizable { }
+extension String:     Customizable { }
+extension Int:        Customizable { }
+extension Double:     Customizable { }
+extension Float:      Customizable { }
+extension Bool:       Customizable { }
+
+
+
+import Foundation
+
+extension NSObject: Customizable { }
+extension Date:     Customizable { }
+
+
+
+#if !os(Linux)
+extension CGPoint:  Customizable { }
+extension CGRect:   Customizable { }
+extension CGSize:   Customizable { }
+extension CGVector: Customizable { }
+#endif
+
+
+
+#if os(iOS) || os(tvOS)
+import UIKit.UIGeometry
+extension UIEdgeInsets: Customizable { }
+extension UIOffset:     Customizable { }
+extension UIRectEdge:   Customizable { }
+#endif
