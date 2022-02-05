1250e61bad3e66ac4dad6eb66a72131e9d2370dd
Asio Otus <asio.otus.verus@icloud.com>
2022-02-05 20:35:41 +0200

    Update URLResponse converter

--- Sources/Core/Specified/StringConverters/URLResponseStringConverters/Implementation/URLResponseStringConverters.ShortSingleLine.swift
+++ Sources/Core/Specified/StringConverters/URLResponseStringConverters/Implementation/URLResponseStringConverters.ShortSingleLine.swift
@@ -2,6 +2,8 @@ import Foundation
 
 extension URLResponseStringConverters {
 	public struct ShortSingleLine: URLResponseStringConverter {
+		public init () { }
+		
 		public func convert (_ urlResponse: URLResponse, body: Data?) -> String {
 			let string = urlResponse.url?.absoluteString ?? "[No URL]"
 			return string
