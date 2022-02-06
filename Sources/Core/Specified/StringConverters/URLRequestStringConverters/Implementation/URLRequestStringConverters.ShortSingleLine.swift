5262d4be1cbaedc630abda4ba906992c902984ec
Asio Otus <asio.otus.verus@icloud.com>
2022-02-06 15:27:33 +0200

    Update

--- Sources/Core/Specified/StringConverters/URLRequestStringConverters/Implementation/URLRequestStringConverters.ShortSingleLine.swift
+++ Sources/Core/Specified/StringConverters/URLRequestStringConverters/Implementation/URLRequestStringConverters.ShortSingleLine.swift
@@ -2,6 +2,8 @@ import Foundation
 
 extension URLRequestStringConverters {
 	public struct ShortSingleLine: URLRequestStringConverter {
+		public init () { }
+		
 		public func convert (_ urlRequest: URLRequest) -> String {
 			let string = "\(urlRequest.httpMethod ?? "[No method]") – \(urlRequest.url?.absoluteString ?? "[No URL]")"
 			return string
