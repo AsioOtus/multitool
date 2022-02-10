43b3d7e6d00cbb548733ee234a9c6627fe629f7d
Asio Otus <asio.otus.verus@icloud.com>
2022-02-10 15:06:12 +0200

    Upgrade

--- Sources/Core/Specified/StringConverters/DictionaryStringConverters/Implementation/DictionaryStringConverters.Multiline.swift
+++ Sources/Core/Specified/StringConverters/DictionaryStringConverters/Implementation/DictionaryStringConverters.Multiline.swift
@@ -4,6 +4,8 @@ extension DictionaryStringConverters {
 	public struct Multiline: DictionaryStringConverter {
 		public static let `default` = Self()
 		
+		public init () { }
+		
 		public func convert (_ dictionary: Dictionary<AnyHashable, Any>) -> String {
 			dictionary.map{ "\($0.key): \($0.value)" }.joined(separator: "\n")
 		}
