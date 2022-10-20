5091a4be6beb096ae2aad4d1789b1291bb52043e
Asio Otus <asio.otus.verus@icloud.com>
2022-10-21 00:42:14 +0300

    U

--- Sources/Core/ResultBuilders/SwitchBlock.swift
+++ Sources/Core/ResultBuilders/SwitchBlock.swift
@@ -1,6 +1,6 @@
 @resultBuilder
-struct SwitchBlock {
-  static func buildBlock <T> (_ t: T) -> T { t }
-  static func buildEither <T> (first: T) -> T { first }
-  static func buildEither <T> (second: T) -> T { second }
+public struct SwitchBlock {
+  public static func buildBlock <T> (_ t: T) -> T { t }
+  public static func buildEither <T> (first: T) -> T { first }
+  public static func buildEither <T> (second: T) -> T { second }
 }
