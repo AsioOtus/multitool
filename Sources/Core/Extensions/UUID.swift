aa82332a341f4a9e9f26d254c2e626e0c9e8a685
Asio Otus <asio.otus.verus@icloud.com>
2023-04-27 01:48:08 +0300

    Add UUID initializer

---
+++ Sources/Core/Extensions/UUID.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public extension UUID {
+  static func create (_ e: Int) -> Self { create(0, 0, 0, 0, e) }
+  static func create (_ d: Int, _ e: Int) -> Self { create(0, 0, 0, d, e) }
+  static func create (_ c: Int, _ d: Int, _ e: Int) -> Self { create(0, 0, c, d, e) }
+  static func create (_ b: Int, _ c: Int, _ d: Int, _ e: Int) -> Self { create(0, b, c, d, e) }
+
+  static func create (_ a: Int, _ b: Int, _ c: Int, _ d: Int, _ e: Int) -> Self {
+    func pad (_ int: Int, size: Int) -> String {
+      String(int).padded(atStartTo: size, with: "0")
+    }
+
+    return .init(uuidString: "\(pad(a, size: 8))-\(pad(b, size: 4))-\(pad(c, size: 4))-\(pad(d, size: 4))-\(pad(e, size: 12))")!
+  }
+}
