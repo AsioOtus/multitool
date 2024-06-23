6c9e1df6245241741140f2c5fc71e85df084c950
Asio Otus <asio.otus.verus@icloud.com>
2024-06-23 20:05:34 +0300

    Add color struct

---
+++ Sources/Core/Structs/Color.swift
@@ -0,0 +1,18 @@
+public struct Color {
+	public let red: Double
+	public let green: Double
+	public let blue: Double
+	public let alpha: Double
+
+	public init (
+		red: Double,
+		green: Double,
+		blue: Double,
+		alpha: Double
+	) {
+		self.red = red
+		self.green = green
+		self.blue = blue
+		self.alpha = alpha
+	}
+}
