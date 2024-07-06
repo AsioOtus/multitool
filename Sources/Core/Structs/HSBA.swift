519aee58844d6d046cb70d49b67e4f04b54fb088
Asio Otus <asio.otus.verus@icloud.com>
2024-07-06 17:21:12 +0300

    Update colors

--- Sources/Core/Structs/HSBA.swift
+++ Sources/Core/Structs/HSBA.swift
@@ -1,4 +1,10 @@
-public struct HSBA: Codable, Hashable {
+import Foundation
+
+public struct HSBA: Codable, Hashable  {
+	public static let black = Self(hue: 0, saturation: 0, brightness: 0)
+	public static let white = Self(hue: 1, saturation: 1, brightness: 1)
+	public static let transparent = Self(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+
 	public let hue: Double
 	public let saturation: Double
 	public let brightness: Double
@@ -27,12 +33,67 @@ public extension HSBA {
 		func adjust (_ value: Double, _ multiplier: Double) -> Double {
 			min(value + value * multiplier, 1)
 		}
-		
+
 		return .init(
 			hue: adjust(self.hue, hue),
 			saturation: adjust(self.saturation, saturation),
 			brightness: adjust(self.brightness, brightness),
 			alpha: adjust(self.alpha, alpha)
 		)
+	}
+}
+
+public extension HSBA {
+	static func rgba (
+		hue: Double,
+		saturation: Double,
+		brightness: Double,
+		alpha: Double
+	) -> RGBA {
+		guard saturation != 0 else {
+			return .init(
+				red: brightness,
+				green: brightness,
+				blue: brightness,
+				alpha: alpha
+			)
+		}
+
+		let angle = (hue >= 360 ? 0 : hue)
+		let sector = angle / 60
+		let i = floor(sector)
+		let f = sector - i
+
+		let p = brightness * (1 - saturation)
+		let q = brightness * (1 - (saturation * f))
+		let t = brightness * (1 - (saturation * (1 - f)))
+
+		return switch(i) {
+		case 0:
+				.init(red: brightness, green: t, blue: p, alpha: alpha)
+		case 1:
+				.init(red: q, green: brightness, blue: p, alpha: alpha)
+		case 2:
+				.init(red: p, green: brightness, blue: t, alpha: alpha)
+		case 3:
+				.init(red: p, green: q, blue: brightness, alpha: alpha)
+		case 4:
+				.init(red: t, green: p, blue: brightness, alpha: alpha)
+		default:
+				.init(red: brightness, green: p, blue: q, alpha: alpha)
+		}
+	}
+
+	static func rgba (hsba: HSBA) -> RGBA {
+		rgba(
+			hue: hsba.hue,
+			saturation: hsba.saturation,
+			brightness: hsba.brightness,
+			alpha: hsba.alpha
+		)
+	}
+
+	var rgba: RGBA {
+		HSBA.rgba(hsba: self)
 	}
 }
--- Sources/Core/Structs/RGBA.swift
+++ Sources/Core/Structs/RGBA.swift
@@ -1,4 +1,8 @@
-public struct RGBA {
+public struct RGBA: Codable, Hashable {
+	public static let black = Self(red: 0, green: 0, blue: 0)
+	public static let white = Self(red: 1, green: 1, blue: 1)
+	public static let transparent = Self(red: 0, green: 0, blue: 0, alpha: 0)
+
 	public let red: Double
 	public let green: Double
 	public let blue: Double
@@ -28,4 +32,50 @@ public extension RGBA {
 	}
 }
 
-extension RGBA: Codable, Hashable { }
+public extension RGBA {
+	static func hsba (
+		red: Double,
+		green: Double,
+		blue: Double,
+		alpha: Double
+	) -> HSBA {
+		let min = red < green ? (red < blue ? red : blue) : (green < blue ? green : blue)
+		let max = red > green ? (red > blue ? red : blue) : (green > blue ? green : blue)
+
+		let brightness = max
+		let delta = max - min
+
+		guard delta > 0.00001 else { return .init(hue: 0, saturation: 0, brightness: max, alpha: alpha) }
+		guard max > 0 else { return .init(hue: -1, saturation: 0, brightness: brightness, alpha: alpha) }
+
+		let saturation = delta / max
+
+		let hueCalculation: (Double, Double) -> Double = { max, delta -> Double in
+			if red == max {
+				(green - blue) / delta
+			} else if green == max {
+				2 + (blue - red) / delta
+			} else {
+				4 + (red - green) / delta
+			}
+		}
+
+		let hue = hueCalculation(max, delta) * 60
+		let adjustedHue = (hue < 0 ? hue + 360 : hue)
+
+		return .init(hue: adjustedHue, saturation: saturation, brightness: brightness, alpha: alpha)
+	}
+
+	static func hsba (rgba: RGBA) -> HSBA {
+		hsba(
+			red: rgba.red,
+			green: rgba.green,
+			blue: rgba.blue,
+			alpha: rgba.alpha
+		)
+	}
+
+	var hsba: HSBA {
+		RGBA.hsba(rgba: self)
+	}
+}
