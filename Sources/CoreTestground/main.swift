f98142ddf5e984e554220379668fc74c830c989b
Anton Pletnev <antons.pletnevs@agcsoft.com>
2025-01-07 09:57:48 +0200

    Clear main file

--- Sources/CoreTestground/main.swift
+++ Sources/CoreTestground/main.swift
@@ -1,11 +1,2 @@
 import Multitool
-import Combine
 
-typealias Loadable<Value> = LoadableValue<Value, Error, AnyCancellable>
-
-var l = Loadable<String>.loading()
-l.cancel()
-
-let a = Just(()).sink { }
-a.store(in: &l)
-//a.kek
